a4521fed55c2f5d20f9f447cbb89ccfcb13ca164
Prince Sharma <prince786no.1@gmail.com>
2023-12-24 09:55:10 +0530

    completed

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -180,7 +180,7 @@ const Dashboard = () => {
                 <section className="payouts">
                     <div className="header">
                         <div className="header-heading">
-                            <div className="heading-title">Payouts</div>
+                            <h5 className="heading-title">Payouts</h5>
                             <div className="heading-info">
                                 <div className="info-icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
